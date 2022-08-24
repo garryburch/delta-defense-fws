39f74fda0d09805e971c26f6e62fd71688740215
Garry Burch <garryburch@finishwellstudios.com>
2022-08-24 12:27:36 -0500

    Init

---
+++ data/data.tsx
@@ -0,0 +1,15 @@
+const Data =
+{
+  "cards":
+    [
+      { "title": "Expert Customer Engagement Team", "content": "100% American-based, the Delta Defense Team are expertly trained on all USCCA products and will help you with any questions you may have" },
+      { "title": "Online Community", "content": "Join fun, gun-friendly conversations with more than 80,000 like-minded Americans on our own ad-free social media platform" },
+      { "title": "Free Entry to Annual USCCA Expo", "content": "USCCA Members get FREE entry to the annual USCCA Concealed Carry & Home Defense Expo [Headed to Fort Worth, TX in November 2022!]" },
+      { "title": "24/7 Critical Response Team Emergency Support", "content": "After a self-defense incident, your first call should be to 9-1-1, and your second call will be to this emergency support team who will be your guide through the unanticipated fog of a self‑defense incident." },
+      { "title": "Nationwide Attorney Network", "content": "When choosing your criminal defense attorney, you may select a name you already trust or you can handpick one from our growing, nationwide network of more than 1,200 attorneys." },
+    ],
+  "phoneText": "Here to Help 24/7",
+  "phone": "800-674-9779",
+}
+
+export default Data;
